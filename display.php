8bb3ee1ebe2207a2fa6db1e09419619b4a456d5a
Sarah <sarahhall90@gmail.com>
2015-12-14 02:48:56 +0000

    display.php

--- display.php
+++ display.php
@@ -6,11 +6,14 @@
  * Summary: Created for On The Bit Sim game members, in php and mysql for leaderboards
  */
 
-include_once('functions.php');
+include_once('Functions.php');
 $name = trim(filter_input(INPUT_GET, "name", FILTER_SANITIZE_STRING));
 $results = showPerHorse($name, $dbh);
-$shows = array_unique($results['showName']);
-var_dump($shows);
+function showName($arr){
+	return $arr['showName'];
+}
+$shows = array_map('showName', $results);
+
 $points = points($results);
 
 echo '<h1>'.$name.'</h1>';
@@ -18,9 +21,34 @@ echo '<h1>'.$name.'</h1>';
 echo "<div id='stats'>
 <b>Total Points: </b> $points<br/>
 </div>
-<div id='results'>
+<div id='results'>";
+	foreach($shows as $show){
+		echo "<h3>$show</h3>
+		<table>
+		<thead>
+		<tr>
+		<th>Class Name</th>
+		<th>Place</th>
+		</tr>
+		</thead>
+		<tbody>";
+			foreach($results as $result){
+				if($result['showName'] == $show){
+					echo "
+						<tr>
+							<td>$result[className]</td>
+							<td>$result[place]</td>
+						</tr>
+						";
+				}
+			}
+		echo "</tbody></table>";
+	}
+	
+	
+echo "
 
-</div>
+</div>";
 
 
 ?>
